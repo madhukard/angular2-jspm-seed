fb59113ff05a67421721b7152a98e83ad8ac968c
Madhukar Devaraju <madhu@streamsets.com>
2015-12-26 10:08:49 -0800

    Fix BrowserSync 404

--- tools/gulp/tasks/develolpment/browser-sync.js
+++ tools/gulp/tasks/develolpment/browser-sync.js
@@ -1,11 +1,12 @@
 var gulp = require('gulp');
 var browsersync = require('browser-sync');
 var config = require('../../config').browsersync.development;
-var url = require('url');
+var historyApiFallback = require('connect-history-api-fallback');
 
 /**
  * Run the build task and start a server with BrowserSync
  */
 gulp.task('browsersync', function () {
+  config.middleware = [historyApiFallback()];
   browsersync(config);
 });
